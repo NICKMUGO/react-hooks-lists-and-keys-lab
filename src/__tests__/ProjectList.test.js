e910af0cdeda1f8864b90606160900019ce293ce
Nick Mugo <nicknjuru13@gmail.com>
2023-08-26 17:02:39 +0300

    Compledted assignment

--- src/__tests__/ProjectList.test.js
+++ src/__tests__/ProjectList.test.js
@@ -34,4 +34,5 @@ test("gives each <ProjectItem> a key based on the project id", () => {
 
 test("renders a <ProjectItem> for each project passed in as a prop", () => {
   render(<ProjectList projects={projects} />);
+
 })
